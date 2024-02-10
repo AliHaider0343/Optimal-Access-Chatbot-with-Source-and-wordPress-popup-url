1e2ebff3e7412ab7f54144fb80c2a61c26e99459
Ali Haider <138858887+AliHaider0343@users.noreply.github.com>
2024-02-10 14:50:18 +0500

    Update DocumentsRetriever.py

--- DocumentsRetriever.py
+++ DocumentsRetriever.py
@@ -9,13 +9,14 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 def retrive_context(query, user_id, chatbot_ID, meta_data=None):
     # Get the absolute path to the current directory
     current_directory = os.getcwd()
-    
     # Specify the persist directory using the absolute path
     persist_directory = os.path.join(current_directory, f"{user_id}-{chatbot_ID}-chroma_db")
     chroma_db = Chroma(persist_directory=persist_directory, embedding_function=OpenAIEmbeddings())
     retriever = chroma_db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.6})
     docs = retriever.get_relevant_documents(query)
     contexts = []
+    context.append(os.listdir())
+
     for document in docs:
         context = {'Context-Information': document.page_content,
                    'Source Link': document.metadata['KuratedContent_sourceUrl'],
