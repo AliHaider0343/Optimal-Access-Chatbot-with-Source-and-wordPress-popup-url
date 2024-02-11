43cc715819d366cccab5ccc01caaca6d0d1356cb
Ali Haider <138858887+AliHaider0343@users.noreply.github.com>
2024-02-11 15:52:18 +0500

    Update DocumentsRetriever.py

--- DocumentsRetriever.py
+++ DocumentsRetriever.py
@@ -6,40 +6,6 @@ import streamlit as st
 import pandas as pd
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-def get_size_and_item(path):
-    # If it's a file, return its size and "file"
-    if os.path.isfile(path):
-        size_bytes = os.path.getsize(path)
-        size_mb = size_bytes / (1024 * 1024)  # Convert bytes to megabytes
-        return size_mb, "file"
-    # If it's a directory, sum up the sizes of all files in the directory recursively and return "directory"
-    elif os.path.isdir(path):
-        total_size_bytes = 0
-        for root, dirs, files in os.walk(path):
-            for f in files:
-                file_path = os.path.join(root, f)
-                total_size_bytes += os.path.getsize(file_path)
-        total_size_mb = total_size_bytes / (1024 * 1024)  # Convert bytes to megabytes
-        return total_size_mb, "directory"
-    else:
-        return 0, None
-
-def list_contents_with_size():
-    # Get the list of all items (files and directories) in the current directory
-    all_contents = os.listdir()
-
-    # Create an empty dictionary to store the results
-    result_dict = {}
-
-    # Loop through each item in the list
-    for item in all_contents:
-        item_path = os.path.join(os.getcwd(), item)
-        size, item_type = get_size_and_item(item_path)
-        result_dict[item] = (size, item_type)
-
-    return result_dict
-
-    
 def retrive_context(query, user_id, chatbot_ID, meta_data=None):
     # Get the absolute path to the current directory
     current_directory = os.getcwd()
@@ -49,8 +15,6 @@ def retrive_context(query, user_id, chatbot_ID, meta_data=None):
     retriever = chroma_db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.6})
     docs = retriever.get_relevant_documents(query)
     contexts = []
-    contexts.append(list_contents_with_size())
-
     for document in docs:
         context = {'Context-Information': document.page_content,
                    'Source Link': document.metadata['KuratedContent_sourceUrl'],
