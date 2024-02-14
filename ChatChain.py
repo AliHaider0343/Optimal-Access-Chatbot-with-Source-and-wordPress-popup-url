e020a3120d98b9128a7d04d8b5822383e5d252e4
Ali Haider <138858887+AliHaider0343@users.noreply.github.com>
2024-02-14 15:54:40 +0500

    Update ChatChain.py

--- ChatChain.py
+++ ChatChain.py
@@ -28,13 +28,11 @@ Generate a comprehensive and informative answer of 80 words or less for the \
 given question based solely on the provided search results (URL and content). You must \
 only use information from the provided search results. Use an unbiased and \
 journalistic tone. Combine search results together into a coherent answer. Do not \
-repeat text. Cite search results using [${{number}}] notation. Only cite the most \
+repeat text. Cite search results using [${{number}}] and [${{Time Period}}] notation. Only cite the most \
 relevant results that answer the question accurately. Place these citations at the end \
 of the sentence or paragraph that reference them - do not put them all at the end. If \
 different results refer to different entities within the same name, write separate \
 answers for each entity.
-
-Attach the Time period of the Trasncript where they apply in the Responce if context have the Time period Tags.
 
 You should use bullet points in your answer for readability. Put citations where they apply
 rather than putting them all at the end.
@@ -48,7 +46,7 @@ bank, not part of the conversation with the user.
     {context} 
 <context/>
 
-MUST REMEMBER: Do not answer question on your own Must Refer to the Context If there is no relevant information within the context, just say "Sorry for Inconvenice, i dont have any Information about it in my Digital Brain." Don't try to make up an answer. Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, not part of the conversation with the user. Must Attach the Time Tag present in the Context where applicable You are a helpful AI Assistant. Respond to the Greeting Messages Properly."""
+MUST REMEMBER: Do not answer question on your own Must Refer to the Context If there is no relevant information within the context, just say "Sorry for Inconvenice, i dont have any Information about it in my Digital Brain." Don't try to make up an answer. Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, not part of the conversation with the user. You are a helpful AI Assistant. Respond to the Greeting Messages Properly."""
 REPHRASE_TEMPLATE = """\
 Given the following conversation and a follow up question, rephrase the follow up \
 question to be a standalone question.
