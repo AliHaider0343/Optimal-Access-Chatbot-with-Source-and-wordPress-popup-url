d058480322c66f694e2027ed3f3a360cf124c0da
Ali Haider <138858887+AliHaider0343@users.noreply.github.com>
2024-02-14 15:42:48 +0500

    Update ChatChain.py

--- ChatChain.py
+++ ChatChain.py
@@ -163,7 +163,7 @@ def Get_Conversation_chain(user_id,chatbot_id,query,chat_history,model="gpt-4"):
         llm = ChatOpenAI(
         model=model,
         streaming=True,
-        temperature=0,)
+        temperature=0,  max_tokens=-1)
 
     answer_chain = create_chain(
         llm,
